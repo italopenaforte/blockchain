c5bdc0c97071926f7e5f9a00ba5922272d494b9a
Italo Penaforte <italopenaforte@gmail.com>
2024-01-09 19:26:18 -0300

    refac: remove validation pathloc

--- src/blockchain.py
+++ src/blockchain.py
@@ -61,8 +61,6 @@ class Blockchain:
         parsed_url = urlparse(address)
         if parsed_url.netloc:
             self.nodes.add(parsed_url.netloc)
-        elif parsed_url.path:
-            self.nodes.add(parsed_url.path)
         else:
             raise ValueError("Invalid URL")
 
